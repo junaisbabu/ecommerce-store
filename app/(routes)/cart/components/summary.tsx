a621ead9990a760a9ed4768fd4325a4a440a62ee
Junais Babu <junais@Junaiss-MacBook-Air.local>
2023-11-16 23:37:14 +0530

    Generate prettier format and className sort in all files within ./app directory

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -34,7 +34,7 @@ const Summary = () => {
       `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
       {
         productIds: items.map((item) => item.id),
-      }
+      },
     );
 
     window.location = response.data.url;
