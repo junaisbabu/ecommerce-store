a5568556c093b000f0fb5139a635f1caf38e8079
Junais Babu <junais@Junaiss-MacBook-Air.local>
2023-11-19 17:44:16 +0530

    Add next-auth session interface type

---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: User;
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    mobile_no: string;
+    password: string;
+  }
+}
